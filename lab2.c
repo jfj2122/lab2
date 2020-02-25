bcbdc00557fb343b2595a3b35fa91c4def07042b
Jeff <jfj2122@columbia.edu>
2020-02-25 16:56:53 +0000

    added semicolon

--- lab2.c
+++ lab2.c
@@ -57,7 +57,7 @@ int main()
   /* Draw rows of asterisks across the top and bottom of the screen */
   for (col = 0 ; col < 64 ; col++) {
     fbputchar('*', 0, col);
-    fbputchar('_', 21, col)
+    fbputchar('_', 21, col);
     fbputchar('*', 23, col);
   }
 
