d02386dfb353a378847357fc4b2c13c418dc47ee
jws <jfj2122@columbia.edu>
2020-02-27 18:05:40 -0500

    remove clear

--- lab2.c
+++ lab2.c
@@ -205,11 +205,12 @@ int main()
 	    }
 	  }
 	}
+      
+	clear(23,21,64,0);
+	fbputs(sendbuf, cur_row, 0);
+	hold = sendbuf[buff_col];
+	fbputchar('_', cur_row, cur_col);
       }
-      //clear(23,21,64,0);
-      fbputs(sendbuf, cur_row, 0);
-      hold = sendbuf[buff_col];
-      fbputchar('_', cur_row, cur_col);
       fbputs(keystate, 6, 0);
       fprintf(stderr, "bufpos: %d \nbuffer: %s\n", buff_col, sendbuf);
       if (packet.keycode[0] == 0x29) { /* ESC pressed? */
